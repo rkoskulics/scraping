acbcca834527e4357e6abed27488983085265a55
Rob <robert.koskulics@me.com>
2018-03-12 11:25:30 -0400

    Add main handlebars

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ app.use(bodyParser.json());
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "home" }));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
+// var routes = require("/controllers/");
 
-app.use(routes);
+// app.use(routes);
 
 app.listen(PORT, function () {
     console.log("App now listening at localhost:" + PORT);
