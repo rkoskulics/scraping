7f9b68663297a41e5a3440dfe9d1ef749457d859
Rob <robert.koskulics@me.com>
2018-03-12 14:30:15 -0400

    Added scrape feature

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -5,9 +5,17 @@ console.log("Scraping test");
 request("https://www.reddit.com/r/funny/", function(error, response, html){
     var $ = cheerio.load(html);
     var results = [];
-    $(".title").each(function(i, element){
-        var title = $(element).children("a").find("rel")
+    $(".thing").each(function(i, element){
+        var title = $(element).find(".title").children("a").text();
+        var link = $(element).find(".title").children("a").attr("href");
+        var thumbnail = $(element).find(".thumbnail").children("img").attr("src");
         results.push({ title: title });
+        if(link.includes("/r/funny")) {
+            link = "https://www.reddit.com" + link
+        };
+        results.push({ link: link});
+        thumbnail = "https:" + thumbnail;
+        results.push({ thumbnail: thumbnail});
     })
     console.log(results)
 })
